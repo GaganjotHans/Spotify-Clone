74613e8779e86b9821cc117149e5e0fae8a55d01
Gaganjot <gaganjothans777@gmail.com>
2024-01-19 00:07:51 -0500

    Intial Frontend layout created

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+
+async function getSongs() {
+  let a = await fetch("http://127.0.0.1:5500/songs/");
+  let response = await a.text();
+  let div = document.createElement("div");
+  div.innerHTML = response;
+  let as = div.getElementsByTagName("a");
+  let songs = [];
+  for (let index = 0; index < as.length; index++) {
+    const element = as[index];
+    if (element.href.endsWith(".mp3")) {
+      songs.push(element.href);
+    }
+  }
+
+  return songs;
+}
+
+async function main() {
+let songs = await getSongs();
+console.log(songs);
+}
+
+main();
